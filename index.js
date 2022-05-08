278fb6c0a4093ee0602052454484ecef831f8ead
unknown <ahmednahid1995@gmail.com>
2022-05-08 13:29:37 +0600

    used environment var & connected to db

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oybys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("theDressingRoom").collection("items");
+    // perform actions on the collection object
+    console.log('mongo connected');
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
