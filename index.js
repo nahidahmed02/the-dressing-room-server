54032a3be0baaff75ee42ddd48b63a5cfd100086
unknown <ahmednahid1995@gmail.com>
2022-05-08 14:13:36 +0600

    get api created for all items

--- index.js
+++ index.js
@@ -14,13 +14,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oybys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("theDressingRoom").collection("items");
-    // perform actions on the collection object
-    console.log('mongo connected');
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('theDressingRoom').collection('items');
+
+        // send all items
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
